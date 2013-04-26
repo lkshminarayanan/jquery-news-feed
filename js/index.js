f2efcaaa8dbff71944b3e6c4d81b4f234839dd4c
Lakshmi Narayanan Saravanan <slnarayanan.tvl@gmail.com>
2013-04-26 17:52:51 +0530

    changing page layout

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-$('#dwload').affix({
-    offset: $('#dwload').position()
-});
-
 
 var newsText = {
 		"Aamir Khan on Time's 100 Most Influential People list" : "http://time100.time.com/2013/04/18/time-100/slide/aamir-khan/",
@@ -10,8 +6,16 @@ var newsText = {
 		"IPL : CSK pulls of a thrilling win over Royals" : "http://www.indianexpress.com/picture-gallery/ipl-6-chennai-super-kings-pull-off-thrilling-win-over-rajasthan-royals/2561-1.html"
 }
 
-$('#ex1').newsFeed({
+$(document).ready(function () {
+	$('#product').affix({
+		offset: $('#product').position()
+	});
+	
+	$('#ex1').newsFeed({
 		newsData: newsText,
 		type:1
+	});
+	
+	$('#nav-wrapper').height($("#nav").height());
+	
 });
-
