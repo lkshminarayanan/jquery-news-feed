f6479daf2f5e026ce32a9a7f4f3a71df79ba8bfc
Lakshmi Narayanan <lkshminarayanan@lkshminarayanan-thinkpad.(none)>
2013-12-10 01:11:20 +0530

    Little reformatting of the code.

--- jquery.newsfeed.js
+++ jquery.newsfeed.js
@@ -24,13 +24,13 @@
 			});
 		};
 	
-        var defaults = {
-            newsData: {"Error":'javascript:alert("Error! Please fill in newsData.");'},//default data
+    var defaults = {
+      newsData: {"Error":'javascript:alert("Error! Please fill in newsData.");'},//default data
 			timeInterval: 5000, /*in seconds*/
-            type: 1, /*1-fadeinfadeout*/
+      type: 1, /*1-fadeinfadeout*/
 			targetBlank: true /*Open in new Window or not*/
-        };
-        optionsWithDefaults = $.extend(defaults, options);
+    };
+    optionsWithDefaults = $.extend(defaults, options);
 		
 		$.each(optionsWithDefaults.newsData, function (newsText, newsLink) {
 			newsArr.push({
@@ -38,24 +38,24 @@
 				'link':newsLink
 			});
 		});
-        return this.each(function () {
+    return this.each(function () {
             
 			newsCount = newsArr.length;
 			iNews =0;
 			//the first time
 			divObj.hide();
-            switch (optionsWithDefaults.type) {
-                default:
-                    //fade in fade out
-                    //default transition here
+      switch (optionsWithDefaults.type) {
+        default:
+          //fade in fade out
+          //default transition here
 					divObj.html(getDisplayHTML());
 					divObj.fadeIn(300);
 					setInterval(fadeInfadeOut,optionsWithDefaults.timeInterval);
-                    break;
-            }
-            if (typeof callback == 'function') { //func check
-                callback.call(this); // brings the scope to the callback
-            }
-        });
-    };
+          break;
+      }
+      if (typeof callback == 'function') { //func check
+    	  callback.call(this); // brings the scope to the callback
+    	}
+    });
+	};
 })(jQuery);
